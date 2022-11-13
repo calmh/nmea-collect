329998217487223be7dcbdde8f8115b9cf24fe5c
Jakob Borg <jakob@kastelo.net>
2022-11-13 11:05:23 +0100

    Improve udp-proxy

--- cmd/udp-proxy/udp-proxy.go
+++ cmd/udp-proxy/udp-proxy.go
@@ -1,70 +1,69 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"net"
-
-	"github.com/alecthomas/kong"
+	"strconv"
+	"strings"
 )
 
-var opts struct {
-	Source string   `long:"source" default:":1457" description:"Source port to listen on"`
-	Target []string `long:"target" description:"Target address to forward to"`
-	Quiet  bool     `long:"quiet" description:"whether to print logging info or not"`
+func main() {
+	flag.Parse()
+	if flag.NArg() == 0 {
+		log.Fatal("No forwarding specs given")
+	}
+	for _, f := range flag.Args() {
+		portStr, dst, ok := strings.Cut(f, ":")
+		if !ok {
+			log.Fatal("Invalid forwarding spec:", f)
+		}
+		port, err := strconv.Atoi(portStr)
+		if err != nil {
+			log.Fatal("Invalid port:", portStr)
+		}
+		if err := forward(port, dst); err != nil {
+			log.Fatal(err)
+		}
+	}
+	select {}
 }
 
-func main() {
-	kong.Parse(&opts)
-	sourceAddr, err := net.ResolveUDPAddr("udp", opts.Source)
+func forward(srcPort int, dstAddr string) error {
+	sourceAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", srcPort))
 	if err != nil {
-		log.Fatalln("Could not resolve source address:", opts.Source)
-		return
+		return fmt.Errorf("could not resolve source address %s: %w", fmt.Sprintf(":%d", srcPort), err)
 	}
 
-	var targetAddr []*net.UDPAddr
-	for _, v := range opts.Target {
-		addr, err := net.ResolveUDPAddr("udp", v)
-		if err != nil {
-			log.Fatalln("Could not resolve target address:", v)
-			return
-		}
-		targetAddr = append(targetAddr, addr)
+	destAddr, err := net.ResolveUDPAddr("udp", dstAddr)
+	if err != nil {
+		return fmt.Errorf("could not resolve destination address %s: %w", dstAddr, err)
 	}
 
 	sourceConn, err := net.ListenUDP("udp", sourceAddr)
 	if err != nil {
-		log.Fatalln("Could not listen on address:", opts.Source)
-		return
+		return fmt.Errorf("could not listen on %s: %w", sourceAddr, err)
 	}
 
-	defer sourceConn.Close()
-
-	var targetConn []*net.UDPConn
-	for _, v := range targetAddr {
-		conn, err := net.DialUDP("udp", nil, v)
-		if err != nil {
-			log.Fatalln("Could not connect to target address:", v)
-			return
-		}
-
-		defer conn.Close()
-		targetConn = append(targetConn, conn)
+	destConn, err := net.DialUDP("udp", nil, destAddr)
+	if err != nil {
+		return fmt.Errorf("could not dial destination address %s: %w", destAddr, err)
 	}
 
-	log.Printf(">> Starting udpproxy, Source at %v, Target at %v...", opts.Source, opts.Target)
-
-	for {
+	go func() {
+		log.Printf("Starting udp-proxy %s -> %s", sourceAddr, destAddr)
 		b := make([]byte, 65536)
-		n, _, err := sourceConn.ReadFromUDP(b)
-		if err != nil {
-			log.Println("Could not receive packet:", err)
-			continue
-		}
-
-		for _, v := range targetConn {
-			if _, err := v.Write(b[0:n]); err != nil {
+		for {
+			n, _, err := sourceConn.ReadFromUDP(b)
+			if err != nil {
+				log.Println("Could not receive packet:", err)
+				continue
+			}
+			if _, err := destConn.Write(b[0:n]); err != nil {
 				log.Println("Could not forward packet:", err)
 			}
 		}
-	}
+	}()
+	return nil
 }
