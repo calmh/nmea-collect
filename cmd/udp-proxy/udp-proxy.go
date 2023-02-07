f653d0d4579acd12eac19787432621280d19a0f1
Jakob Borg <jakob@kastelo.net>
2023-02-07 15:27:46 +0100

    Use fewer HTTP POSTs

--- cmd/udp-proxy/udp-proxy.go
+++ cmd/udp-proxy/udp-proxy.go
@@ -121,26 +121,45 @@ func (f *httpForwarder) Serve(ctx context.Context) error {
 	defer sourceConn.Close()
 
 	log.Printf("Proxying UDP %s -> %s", sourceAddr, f.dstAddr)
-	b := make([]byte, 65536)
+	maxBuffer := 65536
+	b := make([]byte, maxBuffer)
+	statsTicker := time.NewTicker(time.Minute)
+	defer statsTicker.Stop()
+	sent, reqs := 0, 0
+	sendSizeThreshold := maxBuffer / 2
+	sendTimeThreshold := 2 * time.Second
+	offset := 0
+	lastSend := time.Now()
 	for {
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
+		case <-statsTicker.C:
+			log.Printf("Stats: sent=%d reqs=%d", sent, reqs)
+			sent = 0
+			reqs = 0
 		default:
 		}
+
 		sourceConn.SetReadDeadline(time.Now().Add(readTimeout))
-		n, _, err := sourceConn.ReadFromUDP(b)
+		n, _, err := sourceConn.ReadFromUDP(b[offset:])
 		if err != nil {
 			return fmt.Errorf("receive packet: %w", err)
 		}
+		offset += n
+
+		if offset < sendSizeThreshold && time.Since(lastSend) < sendTimeThreshold {
+			continue
+		}
 
 		ctx, cancel := context.WithDeadline(ctx, time.Now().Add(httpTimeout))
-		defer cancel()
-		req, err := http.NewRequestWithContext(ctx, http.MethodPost, f.dstAddr, bytes.NewReader(b[0:n]))
+		req, err := http.NewRequestWithContext(ctx, http.MethodPost, f.dstAddr, bytes.NewReader(b[:offset]))
 		if err != nil {
+			cancel()
 			return fmt.Errorf("create HTTP request: %w", err)
 		}
 		resp, err := http.DefaultClient.Do(req)
+		cancel()
 		if err != nil {
 			return fmt.Errorf("forward packet: %w", err)
 		}
@@ -148,5 +167,10 @@ func (f *httpForwarder) Serve(ctx context.Context) error {
 		if resp.StatusCode != http.StatusOK {
 			return fmt.Errorf("forward packet: %s", resp.Status)
 		}
+
+		sent += offset
+		reqs++
+		offset = 0
+		lastSend = time.Now()
 	}
 }
